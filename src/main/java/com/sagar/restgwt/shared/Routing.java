6075f9281909f273a70e966e9bb0734df8c01e2a
Korbel Daniel <it@suprabrokers.pl>
2015-12-23 15:49:16 +0100

    Working rest services

--- src/main/java/com/sagar/restgwt/shared/Routing.java
+++ src/main/java/com/sagar/restgwt/shared/Routing.java
@@ -2,6 +2,7 @@ package com.sagar.restgwt.shared;
 
 public class Routing {
 	private static final String separator = "/";
+
 	public static class TestService {
 		private static final String testService = "testService" + separator;
 		public static final String getInfo = testService + "getInfo";
